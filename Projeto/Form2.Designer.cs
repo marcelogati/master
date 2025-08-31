3c481433045ea4f2d873044d389eace2b3e1cee7
marcelogati <marcelo.hgati@gmail.com>
2025-08-31 14:32:32 -0300

    criação de banco, tabela

--- Projeto/Form2.Designer.cs
+++ Projeto/Form2.Designer.cs
@@ -28,76 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblNomeUsuario = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtNomeUsuario = new System.Windows.Forms.TextBox();
+            this.txtEmailUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenhaUsuario = new System.Windows.Forms.TextBox();
             this.btnGravar = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
+            this.txtIdadeUsuario = new System.Windows.Forms.TextBox();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.txtCodUsuario = new System.Windows.Forms.TextBox();
+            this.lblCodUsuario = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lblNomeUsuario
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(65, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nome";
+            this.lblNomeUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeUsuario.Location = new System.Drawing.Point(20, 39);
+            this.lblNomeUsuario.Name = "lblNomeUsuario";
+            this.lblNomeUsuario.Size = new System.Drawing.Size(69, 20);
+            this.lblNomeUsuario.TabIndex = 0;
+            this.lblNomeUsuario.Text = "Nome";
             // 
-            // label2
+            // lblEmail
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(65, 101);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 26);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Email";
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(20, 65);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(69, 26);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
             // 
-            // label3
+            // lblSenha
             // 
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(65, 145);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 43);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Senha";
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(20, 91);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(80, 26);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha";
             // 
-            // textBox1
+            // txtNomeUsuario
             // 
-            this.textBox1.Location = new System.Drawing.Point(140, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(215, 22);
-            this.textBox1.TabIndex = 3;
+            this.txtNomeUsuario.Location = new System.Drawing.Point(160, 37);
+            this.txtNomeUsuario.Name = "txtNomeUsuario";
+            this.txtNomeUsuario.Size = new System.Drawing.Size(215, 22);
+            this.txtNomeUsuario.TabIndex = 3;
             // 
-            // textBox2
+            // txtEmailUsuario
             // 
-            this.textBox2.Location = new System.Drawing.Point(140, 101);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(215, 22);
-            this.textBox2.TabIndex = 4;
+            this.txtEmailUsuario.Location = new System.Drawing.Point(160, 65);
+            this.txtEmailUsuario.Name = "txtEmailUsuario";
+            this.txtEmailUsuario.Size = new System.Drawing.Size(215, 22);
+            this.txtEmailUsuario.TabIndex = 4;
             // 
-            // textBox3
+            // txtSenhaUsuario
             // 
-            this.textBox3.Location = new System.Drawing.Point(140, 145);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(215, 22);
-            this.textBox3.TabIndex = 5;
+            this.txtSenhaUsuario.Location = new System.Drawing.Point(160, 91);
+            this.txtSenhaUsuario.Name = "txtSenhaUsuario";
+            this.txtSenhaUsuario.Size = new System.Drawing.Size(215, 22);
+            this.txtSenhaUsuario.TabIndex = 5;
             // 
             // btnGravar
             // 
-            this.btnGravar.Location = new System.Drawing.Point(140, 191);
+            this.btnGravar.Location = new System.Drawing.Point(160, 168);
             this.btnGravar.Name = "btnGravar";
             this.btnGravar.Size = new System.Drawing.Size(75, 23);
             this.btnGravar.TabIndex = 6;
             this.btnGravar.Text = "Gravar";
             this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
             // 
             // btnSair
             // 
-            this.btnSair.Location = new System.Drawing.Point(232, 191);
+            this.btnSair.Location = new System.Drawing.Point(260, 168);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(78, 23);
             this.btnSair.TabIndex = 7;
@@ -105,19 +110,55 @@
             this.btnSair.UseVisualStyleBackColor = true;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
+            // txtIdadeUsuario
+            // 
+            this.txtIdadeUsuario.Location = new System.Drawing.Point(160, 121);
+            this.txtIdadeUsuario.Name = "txtIdadeUsuario";
+            this.txtIdadeUsuario.Size = new System.Drawing.Size(215, 22);
+            this.txtIdadeUsuario.TabIndex = 9;
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.Location = new System.Drawing.Point(20, 117);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(80, 43);
+            this.lblIdade.TabIndex = 8;
+            this.lblIdade.Text = "Idade";
+            // 
+            // txtCodUsuario
+            // 
+            this.txtCodUsuario.Location = new System.Drawing.Point(160, 9);
+            this.txtCodUsuario.Name = "txtCodUsuario";
+            this.txtCodUsuario.Size = new System.Drawing.Size(215, 22);
+            this.txtCodUsuario.TabIndex = 11;
+            // 
+            // lblCodUsuario
+            // 
+            this.lblCodUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodUsuario.Location = new System.Drawing.Point(20, 9);
+            this.lblCodUsuario.Name = "lblCodUsuario";
+            this.lblCodUsuario.Size = new System.Drawing.Size(134, 20);
+            this.lblCodUsuario.TabIndex = 10;
+            this.lblCodUsuario.Text = "Cod. Usuario";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 246);
+            this.ClientSize = new System.Drawing.Size(420, 237);
+            this.Controls.Add(this.txtCodUsuario);
+            this.Controls.Add(this.lblCodUsuario);
+            this.Controls.Add(this.txtIdadeUsuario);
+            this.Controls.Add(this.lblIdade);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnGravar);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenhaUsuario);
+            this.Controls.Add(this.txtEmailUsuario);
+            this.Controls.Add(this.txtNomeUsuario);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNomeUsuario);
             this.Name = "Form2";
             this.Text = "Cadastro de Usuário";
             this.ResumeLayout(false);
@@ -127,13 +168,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblNomeUsuario;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtNomeUsuario;
+        private System.Windows.Forms.TextBox txtEmailUsuario;
+        private System.Windows.Forms.TextBox txtSenhaUsuario;
         private System.Windows.Forms.Button btnGravar;
         private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtIdadeUsuario;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.TextBox txtCodUsuario;
+        private System.Windows.Forms.Label lblCodUsuario;
     }
 }
